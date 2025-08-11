960c8e9bda18fb622b825654db3dcdd9b60b8334
Diego Pineda <zentoo31@gmail.com>
2025-08-11 18:29:43 -0500

    refactor: convert module exports to ES module syntax for URL constants

--- src/util/urls.js
+++ src/util/urls.js
@@ -1,13 +1,11 @@
-module.exports = {
-    BASE_URL: 'https://tioanime.com/',
-    BROWSE_URL: 'https://tioanime.com/directorio?year=1950%2C2021&status=1&sort=recent&type%5B%5D=0',
-    DETAIL_URL: 'https://tioanime.com/anime/',
-    IMAGE_URL: 'https://tioanime.com/uploads/thumbs/',
-    WATCH_URL: 'https://tioanime.com/ver/',
-    SEARCH_URL: 'https://tioanime.com/directorio?q=',
-    GENRE_URL: 'https://tioanime.com/directorio?genero%5B%5D=',
-    GENRES: ['accion','aventura','carreras','comedia','demencia','demonios','deportes','drama','ecchi','escolares','espacial',
+export const BASE_URL = 'https://tioanime.com/';
+export const BROWSE_URL = 'https://tioanime.com/directorio?year=1950%2C2021&status=1&sort=recent&type%5B%5D=0';
+export const DETAIL_URL = 'https://tioanime.com/anime/';
+export const IMAGE_URL = 'https://tioanime.com/uploads/thumbs/';
+export const WATCH_URL = 'https://tioanime.com/ver/';
+export const SEARCH_URL = 'https://tioanime.com/directorio?q=';
+export const GENRE_URL = 'https://tioanime.com/directorio?genero%5B%5D=';
+export const GENRES = ['accion','aventura','carreras','comedia','demencia','demonios','deportes','drama','ecchi','escolares','espacial',
     'fantasia','harem','historico','infantil','josei','juegos','magia','mecha','militar','misterio','musica','parodia','policia','psicologico',
-    'romance','samurai','seinen','shoujo','shounen','sobrenatural','superpoderes','suspenso','terror','vampiros','yaoi','yuri'],
-    JIKAN_URL: 'https://api.jikan.moe/v3/anime/',
-};
+    'romance','samurai','seinen','shoujo','shounen','sobrenatural','superpoderes','suspenso','terror','vampiros','yaoi','yuri'];
+export const JIKAN_URL = 'https://api.jikan.moe/v3/anime/';
